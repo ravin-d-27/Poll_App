727e71b701c265eba04b97095454d23c22301359
Ravin D <ravin.d3107@outlook.com>
2023-12-12 21:03:50 +0530

    Added the vote function. Start with results

--- Polls_App/polls/views.py
+++ Polls_App/polls/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import Http404, HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 
 from django.template import loader
+from django.urls import reverse
 # Create your views here.
 
-from .models import Question
+from .models import Question, Choice
 
 def index(request):
     latest_question_list= Question.objects.order_by('-pub_text')
@@ -16,7 +17,16 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question':question})
 
 def vote(request, question_id):
-    return render(request, 'polls/vote.html')
+    question = get_object_or_404(Question, pk=question_id)
+    
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/vote.html', {'question':question, 'error_message':'You did not select any choice'})
+    else:
+        selected_choice.votes+=1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
 
 def results(request, question_id):
     return render(request, 'polls/results.html')
