3d2de207a8747716fc8482fe4c4f8155ad2d60ca
Ravin D <ravin.d3107@outlook.com>
2023-12-12 20:30:56 +0530

    Added the Home.html

--- Polls_App/polls/admin.py
+++ Polls_App/polls/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from models import Question
+from .models import Question
 # Register your models here.
 admin.site.register(Question)
--- Polls_App/polls/views.py
+++ Polls_App/polls/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from django.template import loader
 # Create your views here.
 
+from .models import Question
+
 def index(request):
-    return render(request, 'polls/home.html')
+    latest_question_list= Question.objects.order_by('-pub_text')
+    context = {'latest_question_list':latest_question_list}
+    return render(request, 'polls/home.html', context)
+
+def detail(request, question_id):
+    return render(request, 'polls/detail.html')
+
+def results(request, question_id):
+    return render(request, 'polls/results.html')
+
+def vote(request, question_id):
+    return render(request, 'polls/vote.html')
