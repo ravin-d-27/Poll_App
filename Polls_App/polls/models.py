a1d7b2b0fc1d494118d9ff26fc26a47b85bffc76
Ravin D <ravin.d3107@outlook.com>
2023-12-12 19:57:03 +0530

    Added the essential models

--- Polls_App/polls/models.py
+++ Polls_App/polls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=300)
+    pub_text = models.DateTimeField('date published')
+    
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choices = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
