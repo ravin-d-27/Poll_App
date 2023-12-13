952bbfcf8f3e3c829954642b4c01b9aa96d431fa
Ravin D <ravin.d3107@outlook.com>
2023-12-13 18:25:35 +0530

    Added the code which validates the user to vote once for a question

--- Polls_App/polls/models.py
+++ Polls_App/polls/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils import timezone
 import datetime
 
+from django.contrib.auth.models import User
+
 # Create your models here.
 
 
@@ -22,3 +24,7 @@ class Choice(models.Model):
     
     def __str__(self):
         return self.choices
+    
+class Vote(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
--- Polls_App/polls/views.py
+++ Polls_App/polls/views.py
@@ -3,6 +3,11 @@ from django.http import Http404, HttpResponse, HttpResponseRedirect
 
 from django.template import loader
 from django.urls import reverse
+
+
+from .models import Question, Choice, Vote
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 from .models import Question, Choice
@@ -12,20 +17,36 @@ def index(request):
     context = {'latest_question_list':latest_question_list}
     return render(request, 'polls/home.html', context)
 
+@login_required
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question':question})
+    
+    # Check if the user has already voted for this question
+    user_has_voted = Vote.objects.filter(user=request.user, question=question).exists()
 
+    context = {'question': question, 'user_has_voted': user_has_voted}
+    return render(request, 'polls/detail.html', context)
+
+@login_required
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+
+    # Check if the user has already voted for this question
+    if Vote.objects.filter(user=request.user, question=question).exists():
+        return render(request, 'polls/vote.html', {'question': question, 'error_message': 'You have already voted for this question'})
+
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        return render(request, 'polls/vote.html', {'question':question, 'error_message':'You did not select any choice'})
+        return render(request, 'polls/vote.html', {'question': question, 'error_message': 'You did not select any choice'})
     else:
-        selected_choice.votes+=1
+        selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results',args=(question_id,)))
+
+        # Save the vote information to ensure the user can't vote again
+        Vote.objects.create(user=request.user, question=question)
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
