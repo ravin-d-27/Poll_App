d5a7ef9c794c22f9ba16bc40c612063d48996730
Ravin D <ravin.d3107@outlook.com>
2023-12-13 18:14:49 +0530

    Configured the Base.html

--- Polls_App/Polls_App/urls.py
+++ Polls_App/Polls_App/urls.py
@@ -21,7 +21,7 @@ from accounts import views as acviews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home', acviews.home, name='home'),
+    path('', acviews.home, name='home'),
     path('login/', acviews.login, name='login'),
     path('signup/', acviews.signup, name='signup'),
     path('logout/', acviews.logout, name='logout'),
