79840b65a228f1989ae2f1eaeeaf9ecdfdba517f
Ravin D <ravin.d3107@outlook.com>
2023-12-13 17:54:55 +0530

    Finished the Vote Part along with results

--- Polls_App/polls/views.py
+++ Polls_App/polls/views.py
@@ -18,7 +18,6 @@ def detail(request, question_id):
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
@@ -26,7 +25,8 @@ def vote(request, question_id):
     else:
         selected_choice.votes+=1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results',args=(question_id,)))
 
 def results(request, question_id):
-    return render(request, 'polls/results.html')
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question':question})
