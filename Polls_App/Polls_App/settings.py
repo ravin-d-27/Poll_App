0c1994b5032bbcb08d197a5182dc10596b3539b0
Ravin D <ravin.d3107@outlook.com>
2023-12-15 22:12:00 +0530

    Added Vercel.json

--- Polls_App/Polls_App/settings.py
+++ Polls_App/Polls_App/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hi57vz@_vo)kyu!w*tp^%kq6ovayd0qshij#-mg=apzl^n8koz'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
